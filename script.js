0aec24c050521062f2826adf6ebb36ea377027ae
tahaeloney <m.taha.baghiha@gmail.com>
2025-12-27 21:04:57 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const faders = document.querySelectorAll('.fade');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  },
+  {
+    threshold: 0.2
+  }
+);
+
+faders.forEach(el => observer.observe(el));
